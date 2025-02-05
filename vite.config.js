3d61bb464f02cbd45401d9306dc9f3daa6d745ca
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-02-05 15:05:57 +0100

    base

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		hmr: {
+			protocol: "ws",
+			host: "localhost",
+		},
+		open: true,
+		port: 3000,
+	},
+});
