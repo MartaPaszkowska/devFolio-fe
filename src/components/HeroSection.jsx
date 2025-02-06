b037ecb973f9aec5b5b39c59bacfb9b431c12bf6
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-02-06 20:56:58 +0100

    hero start

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,15 @@
 import "../styles/HeroSection.css";
+import profileImage from "../assets/images/CVzdjecie.jpg";
 
 function HeroSection() {
 	return (
 		<section className="hero">
 			<p>Hello world!</p>
-			<h1>Najbardziej na swiecie kocham moje córeczki :D</h1>
+			<div className="hero-image-container">
+				<div className="image-glow"></div>
+				<img src={profileImage} alt="Marta" className="hero-image" />
+			</div>
+			<h1>My name is Marta. I'm a developer specializing in React.</h1>
 
 			<button>Hire Me</button>
 			<button>Download Resume</button>
