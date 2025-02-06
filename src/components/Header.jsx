e2a446011c2550aba781c4676ba476f4817a167f
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-02-06 15:43:48 +0100

    header cd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,10 @@ import { HashLink } from "react-router-hash-link";
 function Header() {
 	return (
 		<header className="header">
+			<div className="logo">
+				<span className="dev">Dev</span>
+				<span className="folio">Folio</span>
+			</div>
 			<nav className="nav">
 				<HashLink smooth to="/#hero">
 					Home
