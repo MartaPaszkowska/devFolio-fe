7f236a35385f5ce7eb80547a6e6765758f0c6316
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-02-06 20:36:48 +0100

    .

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ function HeroSection() {
 	return (
 		<section className="hero">
 			<p>Hello world!</p>
-			<h1>Najbardziej na swiiecie kocham moje córeczki :D</h1>
+			<h1>Najbardziej na swiecie kocham moje córeczki :D</h1>
 
 			<button>Hire Me</button>
 			<button>Download Resume</button>
