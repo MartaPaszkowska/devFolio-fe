ee2d70c4f6279787fa63863253b0ad5951b57c9a
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-02-06 14:46:20 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SharedLayout from "./components/SharedLayout";
 import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ProjectsPage from "./pages/ProjectsPage";
-import ContactPage from "./pages/ContactPage";
 
 function App() {
 	return (
@@ -11,9 +8,6 @@ function App() {
 			<SharedLayout>
 				<Routes>
 					<Route path="/" element={<HomePage />} />
-					<Route path="/about" element={<AboutPage />} />
-					<Route path="/projects" element={<ProjectsPage />} />
-					<Route path="/contact" element={<ContactPage />} />
 				</Routes>
 			</SharedLayout>
 		</Router>
