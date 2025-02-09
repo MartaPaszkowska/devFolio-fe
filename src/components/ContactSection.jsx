5add343a22d9ec66b6cd6d874b2a74ca186227dc
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-02-09 18:42:36 +0100

    contact section style done

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -3,13 +3,29 @@ import "../styles/ContactSection.css";
 function ContactSection() {
 	return (
 		<section className="contact">
-			<h2>Contact Me</h2>
-			<form>
-				<input type="text" placeholder="Your Name" required />
-				<input type="email" placeholder="Your Email" required />
-				<textarea placeholder="Your Message" required></textarea>
-				<button type="submit">Send</button>
-			</form>
+			<div className="contact-content">
+				<div className="contact-text">
+					<h2>Hire Me</h2>
+					<p>
+						Let's collaborate to create perfect and most
+						user-friendly digital products. Just leave a message to
+						get in touch.
+					</p>
+				</div>
+				<form className="contact-form">
+					<input
+						type="text"
+						placeholder="Full Name/Company*"
+						required
+					/>
+					<input type="email" placeholder="E-mail/Tel*" required />
+					<textarea
+						placeholder="How can I help you?*"
+						required
+					></textarea>
+					<button type="submit">Send Message</button>
+				</form>
+			</div>
 		</section>
 	);
 }
