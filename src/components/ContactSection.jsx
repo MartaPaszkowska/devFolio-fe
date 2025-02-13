73885353e48e777c59d9f033fac2d23f3cf8fac9
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-02-13 22:17:26 +0100

    .

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -14,9 +14,9 @@ function ContactSection() {
 		e.preventDefault();
 
 		// Konfiguracja EmailJS
-		const serviceId = "service_myjgg76";
-		const templateId = "template_9ztfql4";
-		const publicKey = "ygUBqJVh5dzoxe5wy";
+		const serviceId = import.meta.env.VITE_REACT_APP_SERVICE_ID;
+		const templateId = import.meta.env.VITE_REACT_APP_TEMPLATE_ID;
+		const publicKey = import.meta.env.VITE_REACT_APP_PUBLIC_KEY;
 
 		// Dane wysyłane do EmailJS
 		const templateParams = {
