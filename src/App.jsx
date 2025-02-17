af8e1db81bd92a2d0c7452c1b1f6634d89f84d1a
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-02-17 13:59:01 +0100

    lazy and suspense have been done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, suspense } from "react";
 
-import HomePage from "./pages/HomePage";
+const HomePage = lazy(() => import("./pages/HomePage"));
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-			</Routes>
-		</Router>
+		<Suspense fallback={<div>Loanding...</div>}>
+			<Router>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+				</Routes>
+			</Router>
+		</Suspense>
 	);
 }
 
